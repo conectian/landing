416204b6ee6592a885481e5bdc908106043a560c
chmolto <chfus@proton.me>
2026-01-14 16:32:04 +0100

    feat: google search icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -65,9 +65,11 @@ export async function generateMetadata(): Promise<Metadata> {
     },
     icons: {
       icon: [
+        { url: "/favicon/favicon.ico", sizes: "48x48" },
         { url: "/favicon/favicon-16x16.png", sizes: "16x16", type: "image/png" },
         { url: "/favicon/favicon-32x32.png", sizes: "32x32", type: "image/png" },
         { url: "/favicon/android-chrome-192x192.png", sizes: "192x192", type: "image/png" },
+        { url: "/favicon/android-chrome-512x512.png", sizes: "512x512", type: "image/png" },
       ],
       shortcut: "/favicon/favicon.ico",
       apple: [
